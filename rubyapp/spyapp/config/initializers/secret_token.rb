06e0b747d548c02cb2e157aaa0237dba6c73ebbd
Jo Cranford <jo.cranford@gmail.com>
2011-06-27 23:08:39 +0100

    Removing old folders

--- rubyapp/spyapp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Spyapp::Application.config.secret_token = '1b7e6c3845448d0b5919c34dcee084a1e6de82320940dc6354448f7530a3b7a99303148e314f928f9d1f6150f68bd82e1c5e463c11a24aa34846fa651b276683'
